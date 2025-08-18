320c1b7da3b2473e7c2b3af648b77d54ae860217
Vikhram Duffour <vikhramd@outlook.fr>
2025-08-18 16:46:19 +0200

    latest update

--- frontheque/src/pages/public/[materialId].js
+++ frontheque/src/pages/public/[materialId].js
@@ -84,7 +84,7 @@ const Page = () => {
                   </Typography>
                   <Divider />
                   <Typography><strong>Salle de stockage:</strong> { materialData.origin } </Typography>
-                  <Typography><strong>Code Commande:</strong> { materialData.codeCommande }</Typography>
+                  <Typography><strong>Code Commande:</strong> { materialData.codeCommande } </Typography>
                   <Typography><strong>Code Barres:</strong> { materialData.codeBarres } </Typography>
                   <Typography><strong>Date d&apos;arrivée:</strong> { materialData.date_arrivee } </Typography>
                 </Stack>
